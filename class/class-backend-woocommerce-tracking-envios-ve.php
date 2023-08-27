54bb4246cb37b6327ce4197d8246b9a49ff1ae74
DESKTOP-LP3L50A\24hwww <24hwww@gmail.com>
2023-08-27 19:35:08 -0300

    -

--- class/class-backend-woocommerce-tracking-envios-ve.php
+++ class/class-backend-woocommerce-tracking-envios-ve.php
@@ -4,6 +4,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 Class Class_Backend_WC_Tracking_Envios_Ve{
     private static $_instance = null;
     public $id_wc_setting = '';
+    public $msg_placeholder = '';
 
     public static function instance() {
         if ( is_null( self::$_instance ) ) {
@@ -12,10 +13,13 @@ Class Class_Backend_WC_Tracking_Envios_Ve{
         return self::$_instance;
     }
     public function __construct() {
-    $this->id_wc_setting = 'shipping';
-    register_activation_hook( __FILE__, function(){
+        $this->id_wc_setting = 'shipping';
 
-    });
+        $this->msg_placeholder = 'Rastreo de envio | Empresa: {empresa} | Codigo: {empresa} | Estado: {empresa}. {fecha}';
+
+        register_activation_hook( __FILE__, function(){
+
+        });
     }
 
     public static function init() {
@@ -29,11 +33,31 @@ Class Class_Backend_WC_Tracking_Envios_Ve{
 
     add_action( 'woocommerce_process_shop_order_meta', [$instance,'save_shop_order_meta_boxes_func'], 10, 2 );
 
+    add_filter('woocommerce_new_order_note_data', [$instance,'woocommerce_new_order_note_data_func'], 10, 2 );    
+
     }
 
     public function section_active_wc_tracking_envios_ve_func($settings_tab){
         $settings_tab[WC_ENVIOS_VE_DOMAIN] = __( WC_ENVIOS_VE_TITLE );
         return $settings_tab;
+    }
+
+    public function etiquetas_tracking($order_id=''){
+        $etiquetas = array(
+            array(
+                'id' => '{empresa}'
+            ),
+            array(
+                'id' => '{codigo}'
+            ),    
+            array(
+                'id' => '{estado}'
+            ),  
+            array(
+                'id' => '{fecha}'
+            ),                                    
+        );
+        return $etiquetas;
     }
 
     public function section_setting_wc_tracking_envios_ve_func($settings, $current_section){
@@ -43,6 +67,13 @@ Class Class_Backend_WC_Tracking_Envios_Ve{
         }
 
         $opciones_empresas = array_column(WC_ENVIOS_VE_EMPRESAS, 'name', 'id');
+
+        $id_etiquetas = array_column($this->etiquetas_tracking(), 'id');
+        $id_etiquetas = implode(', ',$id_etiquetas);
+
+        $desc_mensaje_placeholder = sprintf('Mensaje que se visualizará en el momento de consultar el código de rastreo/tracking. <p><strong>Etiquetas:</strong></p><p>%s</p>', $id_etiquetas);
+
+        $mensaje_placeholder = $this->msg_placeholder;
 
         $plugin_settings =  array(
 
@@ -56,7 +87,7 @@ Class Class_Backend_WC_Tracking_Envios_Ve{
 			array(
 				'name' => __( 'Activar funcionamiento' ),
 				'type' => 'checkbox',
-                'desc_tip' => __( 'Dentro de cada orden o pedido se visualiza un cuadro para escribir código de rastreo o tracking y seleccionar la empresa de encomienda.', 'text-domain' ),
+                'desc_tip' => __( 'Dentro de cada orden o pedido se visualiza un cuadro para escribir código de rastreo o tracking y seleccionar la empresa de encomienda.', 'default' ),
 				'desc' => __( 'Mostrar cuadro para insertar código.'),
 				'id'	=> WC_ENVIOS_VE_DOMAIN .'_enable'
 			),
@@ -73,14 +104,24 @@ Class Class_Backend_WC_Tracking_Envios_Ve{
 			),
 
 			array(
-				'name' => __( 'Mensaje en shortcode' ),
+				'name' => __( 'Mensaje en nota' ),
 				'type' => 'textarea',
-				'desc' => __( 'Mensaje que se visualizará en el momento de consultar el código de rastreo/tracking'),
-				'desc_tip' => true,
+				'desc' => $desc_mensaje_placeholder,
+                'desc_tip' => __( 'Dentro de cada orden o pedido se visualiza un cuadro para escribir código de rastreo o tracking y seleccionar la empresa de encomienda.', 'default' ),
+				'placeholder' => $mensaje_placeholder,
 				'id'	=> WC_ENVIOS_VE_DOMAIN .'_mensaje',
 			),
 
 			array( 'type' => 'sectionend', 'id' => WC_ENVIOS_VE_DOMAIN ),
+
+            array(
+				'name' => __( 'Shortcode' ),
+				'type' => 'title',
+				'desc' => __( 'Coloque este shortcode: <strong>[wc_tracking_ve_consulta]</strong> en la página donde desea mostrar un buscador de código de rastreo.' ),
+				'id'   => WC_ENVIOS_VE_DOMAIN 
+			),
+
+
 	);
 
     return $plugin_settings;
@@ -88,16 +129,18 @@ Class Class_Backend_WC_Tracking_Envios_Ve{
     }
 
     public function funcionamiento_activo(){
-        $if_active = false;
+        $if_activo = false;
         $funcionamiento_activo = get_option(WC_ENVIOS_VE_DOMAIN .'_enable','');
-        if($funcionamiento_activo == 'on'){
-            $if_active = true;
+        if($funcionamiento_activo == 'yes'){
+            $if_activo = true;
         }
         return $if_activo;
     }
 
     public function add_shop_order_meta_boxes_func(){
         $instance = self::instance();
+
+        if($this->funcionamiento_activo() !== true){return false;}
 
         add_meta_box(WC_ENVIOS_VE_DOMAIN, __(WC_ENVIOS_VE_TITLE,'woocommerce'), array($instance,'add_shop_order_meta_boxes_content_func'), 'shop_order', 'side', 'core' );
     }
@@ -129,16 +172,31 @@ Class Class_Backend_WC_Tracking_Envios_Ve{
             array(
                 'id' => WC_ENVIOS_VE_DOMAIN.'_delivered',
                 'name' => __('Entregado','default'),
-            ),                        
+            ),   
+            array(
+                'id' => WC_ENVIOS_VE_DOMAIN.'_stopped',
+                'name' => __('Detenido','default'),
+            ),  
+            array(
+                'id' => WC_ENVIOS_VE_DOMAIN.'_failed',
+                'name' => __('Fallido','default'),
+            ),                                                        
         ];
         $estados_de_envio_filters = apply_filters( 'estados_de_envio_encomienda',$estados_de_envio);
         return $estados_de_envio_filters;
     }
 
     public function add_shop_order_meta_boxes_content_func($post){
-        $order = wc_get_order($post->ID);
+
+        if($this->funcionamiento_activo() !== true){return false;}
+
+        $post_id = $post->ID;
+
+        $order = wc_get_order($post_id);
 
         $opciones_empresas = array_column(WC_ENVIOS_VE_EMPRESAS, 'name', 'id');
+
+        $codigo = $order->get_meta(WC_ENVIOS_VE_DOMAIN.'_codigo');
 
         /* wc_tracking_ve_empresa */
         $empresa = $order->get_meta(WC_ENVIOS_VE_DOMAIN.'_empresa');
@@ -152,12 +210,16 @@ Class Class_Backend_WC_Tracking_Envios_Ve{
         $options_empresas = array_merge([''=>'-- Seleccionar empresa --'],$options_empresas);
 
         /* wc_tracking_ve_codigo */
-        $codigo = $order->get_meta(WC_ENVIOS_VE_DOMAIN.'_codigo');
+        
         $nonce = wp_create_nonce()/* wc_tracking_ve_nonce */;
+
+        $estados = array_column($this->estados_de_envio(), 'name', 'id');
+        $options_estados = array_merge([''=>'-- Seleccionar estado --'],$estados);
+        $estado = $order->get_meta(WC_ENVIOS_VE_DOMAIN.'_estado');
 
         ob_start();
 
-        woocommerce_form_field( 'wc_tracking_ve_empresa', array(
+        woocommerce_form_field( WC_ENVIOS_VE_DOMAIN.'_empresa', array(
             'type'          => 'select',
             'class'         => array('wc_tracking_ve_empresa form-row-wide'),
             'label'         => __('Empresa'),
@@ -165,23 +227,48 @@ Class Class_Backend_WC_Tracking_Envios_Ve{
             'options'     => $options_empresas,
         ), $empresa);
         
-        woocommerce_form_field( 'wc_tracking_ve_codigo', array(
+        woocommerce_form_field( WC_ENVIOS_VE_DOMAIN.'_codigo', array(
             'type'        => 'text',
             'required'    => false,
             'label'       => 'Código de rastreo/tracking',
             'description' => 'Indique el código del envio.',
         ), $codigo );
 
-        echo sprintf('<input type="hidden" name="wc_tracking_ve_nonce" value="%s" />',$nonce);
+        woocommerce_form_field( WC_ENVIOS_VE_DOMAIN.'_estado', array(
+            'type'          => 'select',
+            'class'         => array('wc_tracking_ve_estado form-row-wide'),
+            'label'         => __('Estado de envio'),
+            'required'    => false,
+            'options'     => $options_estados,
+        ), $estado);        
+
+        echo sprintf('<input type="hidden" name="%s_nonce" value="%s" />',WC_ENVIOS_VE_DOMAIN, $nonce);
+
+        $get_order_notes = wc_get_order_notes([
+            'order_id' => $order->get_id(),
+            'type' => 'customer',
+         ]);
+        echo '<pre>';
+        print_r($get_order_notes);
+        echo '</pre>';
 
         $output = ob_get_contents();
         ob_end_clean();
         echo $output;
     }
 
+    public function woocommerce_new_order_note_data_func($comment){
+        $comment_content = $comment['comment_content'];
+        $if_wc_envios = strpos($comment_content,WC_ENVIOS_VE_DOMAIN);
+        if($if_wc_envios !== false){
+            $comment['comment_agent'] = WC_ENVIOS_VE_DOMAIN;
+        }
+        return $comment;
+    }
+
     public function save_shop_order_meta_boxes_func($order_id, $post){
 
-        $wc_tracking_ve_nonce = isset($_REQUEST['wc_tracking_ve_nonce']) ? $_REQUEST['wc_tracking_ve_nonce'] : '';
+        $wc_tracking_ve_nonce = isset($_REQUEST[WC_ENVIOS_VE_DOMAIN.'_nonce']) ? $_REQUEST[WC_ENVIOS_VE_DOMAIN.'_nonce'] : '';
 
         if ( ! wp_verify_nonce($wc_tracking_ve_nonce) ) {
             return $order_id;
@@ -189,12 +276,50 @@ Class Class_Backend_WC_Tracking_Envios_Ve{
 
         $order = wc_get_order($order_id);
 
-        $wc_tracking_ve_empresa = isset($_REQUEST['wc_tracking_ve_empresa']) ? sanitize_text_field($_REQUEST['wc_tracking_ve_empresa']) : '';
-        $wc_tracking_ve_codigo = isset($_REQUEST['wc_tracking_ve_codigo']) ? sanitize_text_field($_REQUEST['wc_tracking_ve_codigo']) : '';
+        $msg = get_option(WC_ENVIOS_VE_DOMAIN .'_mensaje',$this->msg_placeholder);
 
-        $order->update_meta_data('wc_tracking_ve_empresa', $wc_tracking_ve_empresa);
+        $campos = array(
+            array(
+                'name' => 'empresa',
+                'tag' => '{empresa}',
+                'id' => WC_ENVIOS_VE_DOMAIN.'_empresa',
+                'label' => 'Empresa',
+            ),
+            array(
+                'name' => 'codigo',
+                'tag' => '{codigo}',
+                'id' => WC_ENVIOS_VE_DOMAIN.'_codigo',
+                'label' => '{Codigo}',
+            ),
+            array(
+                'name' => 'estado',
+                'tag' => '{estado}',
+                'id' => WC_ENVIOS_VE_DOMAIN.'_estado',
+                'label' => 'Estado',
+            )                                 
+        );
 
-        $order->update_meta_data('wc_tracking_ve_codigo', $wc_tracking_ve_codigo);
+        $etiquetas = [];
+        foreach($campos as $index => $val){
+            $campo = $val['id'];
+            $label = $val['label'];
+            $variable = isset($_REQUEST[$campo]) ? sanitize_text_field($_REQUEST[$campo]) : '';
+
+            $order->update_meta_data($val, $variable);
+
+            $etiquetas[esc_attr($val['tag'])] = $variable;
+        }
+
+        $etiquetas['{fecha}'] = date_i18n( 'Y-m-d' );
+
+        #$txt_orde_note .= ' | '.date_i18n( 'Y-m-d' );
+        
+        $msg_note = str_replace(
+            array_keys($etiquetas),
+            array_values($etiquetas), 
+            $msg);
+
+        $order->add_order_note($msg_note, true );
 
         $order->save();
 
